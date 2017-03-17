810736156b2e1676c31f42236f47ecee1635c8cb
Diego Mónaco <dfmonaco@gmail.com>
2017-03-17 19:53:58 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 cwd=$(pwd)
-files=".ackrc .bash_profile .bashrc .dotrc .gemrc .gitconfig .gitignore_global .irbrc .pryrc .railsrc .rspec .tmux.conf .vimrc .zshrc"
+files=".ackrc .bash_profile .bashrc .gemrc .gitconfig .gitignore_global .irbrc .pryrc .railsrc .tmux.conf .vimrc .zshrc"
 
 ##########
 
@@ -16,6 +16,3 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -f -s $cwd/$file ~/$file
 done
-
-rm -rf ~/.oh-my-zsh/custom
-ln -s $cwd/oh-my-zsh/custom ~/.oh-my-zsh/custom
