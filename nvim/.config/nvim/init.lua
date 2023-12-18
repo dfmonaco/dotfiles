065465768359e9698129a4f64809bacedfa37449
Diego Mónaco <dfmonaco@gmail.com>
2023-12-17 22:14:54 -0300

    improve lsp keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -269,27 +269,48 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = group,
   desc = 'LSP actions',
   callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
+    local bufmap = function(mode, keys, cmd, opts)
+      opts.buffer = true
+      vim.keymap.set(mode, keys, cmd, opts)
     end
 
     -- You can search each function in the help page.
     -- For example :help vim.lsp.buf.hover()
 
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    bufmap({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+    -- It shows information about the symbol under the cursor
+    bufmap('n', '<leader>li', '<cmd>lua vim.lsp.buf.hover()<cr>',
+      {desc = 'Show info'}
+    )
+
+    -- It jumps to the definition of the symbol under the cursor
+    bufmap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>',
+      {desc = 'Go to definition'}
+    )
+
+    -- Lists all the implementations of the symbol under the cursor
+    bufmap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>',
+      {desc = 'List implementations'}
+    )
+
+    -- Lists all the references of the symbol under the cursor in the quickfix
+    bufmap('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>',
+      {desc = 'List references'}
+    )
+
+    -- Displays the signature of the function
+    bufmap('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
+      {desc = 'Show signature'}
+    )
+
+    -- Formats the buffer using the current language server
+    bufmap({'n', 'x'}, '<leader>lf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+      {desc = 'Format buffer'}
+    )
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+      {desc = 'Select code action'}
+    )
   end
 })
 
@@ -620,6 +641,7 @@ require('which-key').register({
   ['<leader>r'] = { name = '[R]eplace' },
   ['<leader>rb'] = { name = '[B]uffers' },
   ['<leader>t'] = { name = '[T]est' },
+  ['<leader>l'] = { name = '[L]sp', _ = 'which_key_ignore' },
 })
 
 ---
