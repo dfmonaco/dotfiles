e32c8febec2357badf8d8b4cc3a031d5d291605b
Diego Mónaco <dfmonaco@gmail.com>
2023-12-19 12:30:03 -0300

    add keymaps for git hunk management and search/replace

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -206,6 +206,10 @@ vim.keymap.set('v', '<C-r>', '<CMD>SearchReplaceSingleBufferVisualSelection<CR>'
   { desc = 'Visual search/replace in buffer' }
 )
 
+vim.keymap.set("v", "<C-s>", "<CMD>SearchReplaceWithinVisualSelection<CR>",
+  { desc = "Search/replace within visual selection" }
+)
+
 vim.keymap.set('n', '<leader>rw', '<CMD>SearchReplaceSingleBufferCWord<CR>',
   { desc = 'Search/replace in buffer for [w]ord' }
 )
@@ -217,6 +221,14 @@ vim.keymap.set('n', '<leader>rW', '<CMD>SearchReplaceSingleBufferCWORD<CR>',
 -- Spectre
 vim.keymap.set('n', '<leader>rp', '<cmd>lua require("spectre").toggle()<CR>', {
     desc = "Toggle Search/Replace [p]anel",
+})
+
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
 })
 
 -- Trouble
@@ -607,6 +619,46 @@ require('gitsigns').setup({
     changedelete = { text = '~' },
     untracked    = { text = '┆' },
   },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', gs.stage_hunk)
+    map('n', '<leader>hr', gs.reset_hunk)
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
 })
 
 
