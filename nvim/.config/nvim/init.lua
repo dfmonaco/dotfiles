3cd6b1fb607c4fda33fac387c11d7dae3511b3eb
Diego Mónaco <dfmonaco@gmail.com>
2023-12-19 14:07:37 -0300

    add desc for gitsigns keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -642,19 +642,41 @@ require('gitsigns').setup({
     end, {expr=true})
 
     -- Actions
-    map('n', '<leader>hs', gs.stage_hunk)
-    map('n', '<leader>hr', gs.reset_hunk)
-    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    map('n', '<leader>hs', gs.stage_hunk,
+      {desc = 'Stage hunk'}
+    )
+    map('n', '<leader>hr', gs.reset_hunk,
+      {desc = 'Reset hunk'}
+    )
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
+      {desc = 'Stage hunk in selection'})
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
+      {desc = 'Reset hunk in selection'}
+    )
+    map('n', '<leader>hS', gs.stage_buffer,
+      {desc = 'Stage buffer'}
+    )
+    map('n', '<leader>hu', gs.undo_stage_hunk,
+      {desc = 'Undo stage hunk'}
+    )
+    map('n', '<leader>hR', gs.reset_buffer,
+      {desc = 'Reset buffer'}
+    )
+    map('n', '<leader>hp', gs.preview_hunk,
+      {desc = 'Preview hunk'}
+    )
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end,
+      {desc = 'Blame line'}
+    )
+    map('n', '<leader>hd', gs.diffthis,
+      {desc = 'Diff this hunk'}
+    )
+    map('n', '<leader>hD', function() gs.diffthis('~') end,
+      {desc = 'Diff this buffer'}
+    )
+    map('n', '<leader>td', gs.toggle_deleted,
+      {desc = 'Toggle deleted hunk'}
+    )
 
     -- Text object
     map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
