22fff51ecf2b4530dc942fb8bab52ffec756861e
Diego Mónaco <dfmonaco@gmail.com>
2024-06-22 15:45:52 -0300

    update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1381,6 +1381,18 @@ else
             require('render-markdown').setup({})
         end,
     },
+    -- { "hinell/lsp-timeout.nvim" } [LSP timeout] {{{3
+    {
+        "hinell/lsp-timeout.nvim",
+        dependencies={ "neovim/nvim-lspconfig" },
+        init = function()
+            vim.g.lspTimeoutConfig = {
+              stopTimeout  = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs 
+              startTimeout = 1000 * 10,     -- ms, timeout before restart
+              silent       = false          -- true to suppress notifications
+            }
+        end
+    },
     -- { "kdheepak/lazygit.vim" } [LazyGit] {{{3
     {
         "kdheepak/lazygit.nvim",
