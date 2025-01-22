de0b3f2af3da92675461fe3e756110f1c2900f4b
Diego Mónaco <dfmonaco@gmail.com>
2025-01-22 14:07:36 -0300

    kitty scrollback vim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1417,6 +1417,19 @@ else
             }
         end
     },
+    -- { 'mikesmithgh/kitty-scrollback.nvim' [Kitty scrollback] {{{3
+    {
+      'mikesmithgh/kitty-scrollback.nvim',
+      enabled = true,
+      lazy = true,
+      cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+      event = { 'User KittyScrollbackLaunch' },
+      -- version = '*', -- latest stable version, may have breaking changes if major version changed
+      -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+      config = function()
+        require('kitty-scrollback').setup()
+      end,
+    },
     -- { "kdheepak/lazygit.vim" } [LazyGit] {{{3
     {
         "kdheepak/lazygit.nvim",
