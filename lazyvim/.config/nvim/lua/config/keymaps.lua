b616a1b6492fa48c226905536efb7ae5f5b5ef68
Diego Mónaco <dfmonaco@gmail.com>
2025-02-09 18:20:34 -0300

    add custom keymaps to lazyvim

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>h", "<C-W>c", { desc = "Delete Window", remap = true })
+vim.keymap.set("n", "<leader>j", "<cmd>w<cr><esc>", { desc = "Save File" })
+vim.keymap.set("n", "<leader>k", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader><leader>", "<c-^>", { desc = "Last buffer" })
