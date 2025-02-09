f88e54254621632b9884aab277c48ef8d14c368b
Diego Mónaco <dfmonaco@gmail.com>
2025-02-09 18:20:54 -0300

    add vim-browser-search plugin

---
+++ lazyvim/.config/nvim/lua/plugins/browser.lua
@@ -0,0 +1,11 @@
+return {
+  "voldikss/vim-browser-search",
+  keys = {
+    {
+      "<C-l>",
+      "<Plug>SearchVisual",
+      mode = "v",
+      desc = "Search visual selection",
+    },
+  },
+}
