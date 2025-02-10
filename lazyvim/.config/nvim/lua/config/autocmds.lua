c1301e583964c46c93cd58576e386976adde6e16
Diego Mónaco <dfmonaco@gmail.com>
2025-02-10 18:48:00 -0300

    autocomand for keywordprg devdocs search

--- lazyvim/.config/nvim/lua/config/autocmds.lua
+++ lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "css", "javascript", "ruby", "sql" },
+  callback = function(args)
+    vim.opt_local.keywordprg = "sh -c 'xdg-open \"https://devdocs.io/search?q= \"$1' _"
+  end,
+})
