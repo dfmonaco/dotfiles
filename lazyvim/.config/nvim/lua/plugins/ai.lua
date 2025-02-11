ed29ca81983691979d8b12d825698f0f6cb96fac
Diego Mónaco <dfmonaco@gmail.com>
2025-02-11 20:45:44 -0300

    configure codecompanion on nvim

--- lazyvim/.config/nvim/lua/plugins/ai.lua
+++ lazyvim/.config/nvim/lua/plugins/ai.lua
@@ -25,4 +25,25 @@ return {
       end,
     },
   },
+  keys = {
+    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+    {
+      "<leader>aa",
+      "<cmd>CodeCompanionChat Toggle<cr>",
+      desc = "Toggle AI Chat",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>am",
+      "<cmd>CodeCompanionActions<cr>",
+      desc = "Open AI Menu",
+      mode = { "n", "v" },
+    },
+    {
+      "ga",
+      "<cmd>CodeCompanionChat Add<cr>",
+      desc = "Add visual selection to AI Chat",
+      mode = { "v" },
+    },
+  },
 }
