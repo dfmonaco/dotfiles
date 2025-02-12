a147e0a48e44dcb00ea6cae378a590beeedd39ee
Diego Mónaco <dfmonaco@gmail.com>
2025-02-12 19:37:06 -0300

    Configure custom prompt in codecompanion plugin

--- lazyvim/.config/nvim/lua/plugins/ai.lua
+++ lazyvim/.config/nvim/lua/plugins/ai.lua
@@ -24,6 +24,38 @@ return {
         })
       end,
     },
+    prompt_library = {
+      ["Proofread"] = {
+        strategy = "chat",
+        description = "Writing assistant for refining texts, emails, and chats",
+        opts = {
+          index = 1,
+          is_default = true,
+          is_slash_cmd = true,
+          short_name = "proofread",
+          ignore_system_prompt = true,
+        },
+        prompts = {
+          {
+            role = "system",
+            content = [[
+You are a Chatbot AI specialized in assisting users with enhancing their
+writing and proofreading skills. Your expertise covers a wide range of text types,
+including general documents, emails, and chat messages.
+Your goal is to provide clear, concise, and actionable feedback
+to help users improve their communication effectiveness.
+            ]],
+          },
+          {
+            role = "user",
+            content = [[
+Please proofread and enhance the following text:
+
+]],
+          },
+        },
+      },
+    },
   },
   keys = {
     { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
