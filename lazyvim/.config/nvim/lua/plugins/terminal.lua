4cb461a2d68d61a800e16d17dc33e3a17a46c037
Diego Mónaco <dfmonaco@gmail.com>
2025-02-12 21:23:38 -0300

    config kitty scrollback vim

--- lazyvim/.config/nvim/lua/plugins/terminal.lua
+++ lazyvim/.config/nvim/lua/plugins/terminal.lua
@@ -39,4 +39,18 @@ return {
       },
     },
   },
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = true,
+    lazy = true,
+    cmd = {
+      "KittyScrollbackGenerateKittens",
+      "KittyScrollbackCheckHealth",
+      "KittyScrollbackGenerateCommandLineEditing",
+    },
+    event = { "User KittyScrollbackLaunch" },
+    config = function()
+      require("kitty-scrollback").setup()
+    end,
+  },
 }
