61c07792056f55015fae5a03d640ac16b963de33
Diego Mónaco <dfmonaco@gmail.com>
2025-02-13 11:52:26 -0300

    change keymap for closing nvim windows

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>h", "<C-W>c", { desc = "Delete Window", remap = true })
 vim.keymap.set("n", "<leader>j", "<cmd>w<cr><esc>", { desc = "Save File" })
 vim.keymap.set("n", "<leader>k", function()
   Snacks.bufdelete()
 end, { desc = "Kill Buffer" })
 vim.keymap.set("n", "<leader><leader>", "<c-^>", { desc = "Last buffer" })
+
+vim.keymap.set("n", "<C-c>", "<C-W>c", { desc = "Delete Window", remap = true })
 vim.keymap.set("t", "<C-Esc>", "<C-\\><C-n>", { desc = "Escape terminal mode" })
 vim.keymap.set("n", "m", "q", { noremap = true, silent = true })
 vim.keymap.set("n", "q", "<Nop>", { noremap = true, silent = true })
