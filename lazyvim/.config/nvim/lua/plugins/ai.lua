fb772104b1eb5540057d9030e1223c5346b22e0f
Diego Mónaco <dfmonaco@gmail.com>
2025-08-22 10:53:19 -0300

    Deactivate mcphub

--- lazyvim/.config/nvim/lua/plugins/ai.lua
+++ lazyvim/.config/nvim/lua/plugins/ai.lua
@@ -6,16 +6,16 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            show_result_in_chat = true,  -- Show mcp tool results in chat
-            make_vars = true,            -- Convert resources to #variables
-            make_slash_commands = true,  -- Add prompts as /slash commands
-          }
-        }
-      },
+      -- extensions = {
+      --   mcphub = {
+      --     callback = "mcphub.extensions.codecompanion",
+      --     opts = {
+      --       show_result_in_chat = true,  -- Show mcp tool results in chat
+      --       make_vars = true,            -- Convert resources to #variables
+      --       make_slash_commands = true,  -- Add prompts as /slash commands
+      --     }
+      --   }
+      -- },
       strategies = {
         chat = {
           adapter = "openai",
@@ -99,11 +99,11 @@ Please proofread and enhance the following text:
       },
     },
   },
-  {
-    "ravitemer/mcphub.nvim",
-    build = "npm install -g mcp-hub@latest",
-    config = function()
-      require("mcphub").setup()
-    end,
-  },
+  -- {
+  --   "ravitemer/mcphub.nvim",
+  --   build = "npm install -g mcp-hub@latest",
+  --   config = function()
+  --     require("mcphub").setup()
+  --   end,
+  -- },
 }
