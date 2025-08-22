2585d3c47007bb28de902d820587754e1e48595a
Diego Mónaco <dfmonaco@gmail.com>
2025-08-22 11:36:15 -0300

    Configure search-replace panel

--- lazyvim/.config/nvim/lua/plugins/search-replace.lua
+++ lazyvim/.config/nvim/lua/plugins/search-replace.lua
@@ -1,24 +1,44 @@
 return {
-  "roobert/search-replace.nvim",
-  keys = {
-    {
-      "<C-r>",
-      "<CMD>SearchReplaceSingleBufferVisualSelection<CR>",
-      mode = "v",
-      desc = "Visual search/replace in buffer"
+  {
+    "roobert/search-replace.nvim",
+    keys = {
+      {
+        "<C-r>",
+        "<CMD>SearchReplaceSingleBufferVisualSelection<CR>",
+        mode = "v",
+        desc = "Visual search/replace in buffer"
+      },
+      {
+        "<leader>rw",
+        "<CMD>SearchReplaceSingleBufferCWord<CR>",
+        desc = "Search/replace in buffer for [w]ord"
+      },
+      {
+        "<leader>rW",
+        "<CMD>SearchReplaceSingleBufferCWORD<CR>",
+        desc = "Search/replace in buffer for [W]WORD"
+      },
     },
-    {
-      "<leader>rw",
-      "<CMD>SearchReplaceSingleBufferCWord<CR>",
-      desc = "Search/replace in buffer for [w]ord"
-    },
-    {
-      "<leader>rW",
-      "<CMD>SearchReplaceSingleBufferCWORD<CR>",
-      desc = "Search/replace in buffer for [W]WORD"
-    },
+    config = function()
+      require("search-replace").setup({})
+    end,
   },
-  config = function()
-    require("search-replace").setup({})
-  end,
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = "GrugFar",
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          grug.open({
+            transient = true,
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+  }
 }
