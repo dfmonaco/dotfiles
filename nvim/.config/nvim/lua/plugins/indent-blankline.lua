326a7660b9ff4d29c08307bda81aba8cb46173b6
Diego Mónaco <dfmonaco@gmail.com>
2025-11-25 16:26:16 -0300

    feat(nvim): add indent-blankline for visual indent guides

---
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    indent = {
+      char = "┊",
+    },
+    scope = {
+      enabled = false,
+    },
+  },
+}
