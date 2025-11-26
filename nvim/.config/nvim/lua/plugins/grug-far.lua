6350ff58050ac5710e806a28be1172f8910991c6
Diego Mónaco <dfmonaco@gmail.com>
2025-11-26 15:30:23 -0300

    update grug-far.nvim keymap

--- nvim/.config/nvim/lua/plugins/grug-far.lua
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -82,7 +82,7 @@ return {
         -- Preview
         preview = "<localleader>p",
         -- Quickfix
-        qflist = "<localleader>q",
+        qflist = "<localleader>f",
       },
       
       -- Window options
