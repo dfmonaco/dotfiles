257a5b8f5763770f7626abdeebd7577517629a79
Diego Mónaco <dfmonaco@gmail.com>
2026-02-01 16:03:16 -0300

    config gitsign preview popup window

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -48,10 +48,12 @@ return {
     max_file_length              = 40000, -- Disable if file is longer than this (in lines)
     preview_config               = {
       -- Options passed to nvim_open_win
+      border = 'rounded',
       style = 'minimal',
-      relative = 'cursor',
-      row = 0,
-      col = 1
+      relative = 'editor',
+      row = math.floor(vim.o.lines * 0.2),
+      col = math.floor(vim.o.columns * 0.2),
+      width = math.floor(vim.o.columns * 0.6),
     },
     -- Keymaps setup function
     on_attach                    = function(bufnr)
