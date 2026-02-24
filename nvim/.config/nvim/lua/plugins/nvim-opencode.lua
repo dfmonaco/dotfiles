2b5deb5d9d8941b4b8815720d5c0bda7c0cb8499
Diego Mónaco <dfmonaco@gmail.com>
2026-02-24 12:38:21 -0300

    Use new nvim-opencode plugin

--- nvim/.config/nvim/lua/plugins/nvim-opencode.lua
+++ nvim/.config/nvim/lua/plugins/nvim-opencode.lua
@@ -2,12 +2,6 @@
 -- Local development plugin
 return {
   {
-    dir = "~/code/nvim.op.old",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
     dir = "~/code/nvim-opencode",
     config = function()
       require("plugin").setup()
